3807fadfbc822e5c08a94d9e6ac0b83dd59f7c57
Fredrik Wallander <f.m.wallander@gmail.com>
2021-01-04 15:35:35 +0100

    start of setup

---
+++ Assets/Scripts/SetupTileTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupTileTypes : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
